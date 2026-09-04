7361f42dcecf5932c1c7984720d8cf27db1cea85
debarkamondal <debarkamondal@gmail.com>
2026-09-04 15:47:06 +0530

    chore: bump version to v0.7.0 and update dependencies

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import type { ExpoConfig } from "expo/config";
 const config: ExpoConfig = {
   name: "DeezChatz",
   slug: "deezchatz",
-  version: "0.6.2",
+  version: "0.7.0",
   orientation: "portrait",
   icon: "./src/assets/images/android-icon-foreground.png",
   scheme: "deezchatz",
@@ -24,7 +24,7 @@ const config: ExpoConfig = {
     },
     predictiveBackGestureEnabled: true,
     package: "in.deez.chatz",
-    versionCode: 5,
+    versionCode: 6,
     googleServicesFile: "./google-services.json",
     blockedPermissions: [
       "android.permission.SYSTEM_ALERT_WINDOW",
