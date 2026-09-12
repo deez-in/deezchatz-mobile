cc92372f0532aac490d87ec9eda8bb552dad4ae4
debarkamondal <debarkamondal@gmail.com>
2026-09-12 10:16:45 +0530

    chore: version bump to 0.8.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to the **DeezChatz Mobile** project will be documented in th
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.8.3] - 2026-09-12
+
+### Added
+- Implemented MQTT connection authentication using the device's Signed Prekey (preKey) to sign the payload.
+
 ## [0.8.2] - 2026-09-11
 
 ### Fixed
--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const withOptionalHardwareFeatures: ConfigPlugin = (config) => {
 const config: ExpoConfig = {
   name: "DeezChatz",
   slug: "deezchatz",
-  version: "0.8.2",
+  version: "0.8.3",
   orientation: "portrait",
   icon: "./src/assets/images/android-icon-foreground.png",
   scheme: "deezchatz",
@@ -64,7 +64,7 @@ const config: ExpoConfig = {
     },
     predictiveBackGestureEnabled: true,
     package: "in.deez.chatz",
-    versionCode: 10,
+    versionCode: 11,
     googleServicesFile: "./google-services.json",
     blockedPermissions: [
       "android.permission.SYSTEM_ALERT_WINDOW",
